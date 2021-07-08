7a3ea385d7b7c849d40aaa9aee536d1cb4527f66
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-07-08 11:59:31 -0300

    fix lint

--- backend/src/modules/auth/strategies/local.strategy.ts
+++ backend/src/modules/auth/strategies/local.strategy.ts
@@ -5,20 +5,20 @@ import { AuthService } from '../auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
-        super({
-            usernameField: 'email',
-            passwordField: 'password',
-        });
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
     }
 
-    async validate(email: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(email, password);
-
-        if(!user) {
-            throw new UnauthorizedException();
-        }
-
-        return user;
-    }
+    return user;
+  }
 }
